2b09fc96cf9b7968fdf9ad0e4d9d445188c482b3
sahan <sahan1989@gmail.com>
2019-07-18 23:50:16 +0200

    Changes

---
+++ src/main/java/com/sahan/graphql/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.sahan.graphql.controller;
+
+import com.sahan.graphql.service.GraphQLService;
+import graphql.ExecutionResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/books")
+@RestController
+public class BookController {
+
+
+    private final GraphQLService graphQLService;
+
+    @Autowired
+    public BookController(GraphQLService graphQLService) {
+        this.graphQLService = graphQLService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ExecutionResult> getAllBooks(@RequestBody String query) {
+        ExecutionResult execute = graphQLService.getGraphQL().execute(query);
+        return new ResponseEntity<>(execute, HttpStatus.OK);
+    }
+}
